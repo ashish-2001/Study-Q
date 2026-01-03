81dbeb5297a118019ec78207affda4b5769049c3
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-03 23:48:39 +0530

    Added types

---
+++ app/actions/types.ts
@@ -0,0 +1,32 @@
+import { CommentType } from "../generated/prisma/enums";
+
+export interface QueryPaams {
+    limit?: number;
+    page?: number;
+    tabType?: number;
+    search?: number;
+    date?: number;
+    type?: number;
+    parentId?: number;
+    userId?: number;
+    commentId?: number;
+    timestamp?: number;
+    editCommentId?: number;
+    newPost?: 'open' | 'close';
+}
+
+export enum TabType {
+    md = 'Most downvotes',
+    mu = 'Most upvotes',
+    mr = 'Most Recent',
+    mq = 'My question'
+}
+
+export type Delete = {
+    message: string
+}
+
+export enum ROLES {
+    ADMIN = 'admin',
+    USER = 'user'
+}
