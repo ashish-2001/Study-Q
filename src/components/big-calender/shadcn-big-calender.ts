1c0e122fe53576f70a7d268a9338d16de881abb4
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-14 23:07:28 +0530

    Added Calender big calender

---
+++ src/components/big-calender/shadcn-big-calender.ts
@@ -0,0 +1,5 @@
+import { Calender } from 'react-big-calender';
+import 'react-big-calender/lib/addons/dragAndDrop/styles.css';
+import './shadcn-big-calender.css';
+export ShadcnBigCalender = Calender;
+export default ShadcnBigCalender;
