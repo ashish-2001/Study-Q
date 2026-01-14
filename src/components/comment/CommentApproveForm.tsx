d54bfbbf45f5f586e1db445af442c3c036891368
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-15 01:03:48 +0530

    Added comment approve form

---
+++ src/components/comment/CommentApproveForm.tsx
@@ -0,0 +1,49 @@
+'use client';
+import { approveComment } from "@/src/actions/comment";
+import { useAction } from '@/hooks/useAction';
+import { CheckIcon } from 'lucide-react';
+import { usePathname } from "next/navigation";
+import React from "react";
+import { toast } from 'sonner';
+
+
+const CommentApproveForm = ({
+    commentId,
+    contentId
+}: {
+    commentId: number;
+    contentId: number;
+}) => {
+    const currentPath = usePathname();
+
+    const { execute, isLoading } = useAction(approveComment, {
+        onSuccess: () => {
+            toast('Comment Approved');
+        },
+        onError: (error) => {
+            toast.error(error);
+        }
+    });
+
+    const handleFormEvent = (E: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        execute({
+            content_comment_ids: `${contentId}; ${commentId}`,
+            approved: true,
+            currentPath
+        });
+    };
+
+    return (
+        <form className="w-full" onSubmit={handleFormEvent}>
+            <button className="w-full" type="submit" disabled={isLoading}>
+                <div className="flex items-center gap-1">
+                    Approve Chapters <CheckIcon className="h-4 w-4"/>
+                </div>
+            </button>
+        </form>
+    )
+}
+
+export default CommentApproveForm;
