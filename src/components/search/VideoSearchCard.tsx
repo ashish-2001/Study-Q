0b237594427d927a7390d5f19d28f2aab096dccf
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-18 23:55:28 +0530

    Added video search card

---
+++ src/components/search/VideoSearchCard.tsx
@@ -0,0 +1,54 @@
+import { TSearchVideos } from "@/src/app/api/mobile/search/route";
+import { Play } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger
+} from '../ui/tooltip';
+import { TSearchedVideos } from "@/src/app/api/search/route";
+
+interface VideoSearchCardProps {
+    video: TSearchedVideos;
+    onCardClick: (videoUrl: string) => void;
+}
+
+const VideoSearchCard: React.FC<VideoSearchCardProps> = ({
+    video,
+    onCardClick
+}) => {
+    const { id: videoId, parentId, parent } = video;
+    if(parentId && parent?.courses.length){
+        const courseId = parent.courses[0].courseId;
+        const videourl = `/courses/${courseId}/${parentId}/${videoId}`;
+        const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+            if(onCardClick){
+                e.preventDefault();
+                onCardClick(videoUrl);
+            }
+        };
+
+        return (
+            <Link href={videoUrl} className="group relative flex cursor-pointer items-center gap-3 rounded-md px-3 py-2 hover:bg-blue-600/10 hover:text-blue-600" onClick={handleClick}>
+                <Play className="size-4"/>
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <span className="w-4.5 truncate font-medium capitalize">
+                                {video.title}
+                            </span>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>{video.title}</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+            </Link>
+        )
+    }
+    return null;
+};
+
+export default VideoSearchCard;
