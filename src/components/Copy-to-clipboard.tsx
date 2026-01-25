278c24f8059181792fbf4f533f390384b77ff5d2
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-25 20:58:14 +0530

    Added course card

---
+++ src/components/Copy-to-clipboard.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { CopyIcon } from 'lucide-react';
+import React from 'react';
+import { toast } from 'sonner';
+
+
+const CopyToClipboard = ({
+    textToCopy
+}: {
+    textToCopy: string;
+    btnLabel?: string;
+}) => {
+    const handleCopyClick = () => {
+        try{
+            navigator.clipboard.writeText(textToCopy);
+            toast.success('Copied to clipboard');
+        } catch(error){
+            toast.error('Failed to copy to clipboard');
+        }
+    };
+
+    return (
+        <div className='w-full'>
+            <button className='flex items-center gap-x-2'>
+                Copy <CopyIcon size={15}/>
+            </button>
+        </div>
+    )
+};
+
+export default CopyToClipboard;
