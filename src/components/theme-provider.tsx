aa57ccfd7e2feda4ad5d75e44fd8aef72554f792
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-30 23:35:47 +0530

    Added theme provider

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ThemeProvider, as NextThemeProvider } from 'next-themes';
+import { ThemeProvider as NextThemeProvider } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps){
