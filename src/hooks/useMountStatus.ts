128ad8df2f16c289d9923055bf409e438b08c772
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-01 00:34:00 +0530

    Added use mount status

---
+++ src/hooks/useMountStatus.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+
+const useMountStatus = () => {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    return mounted;
+};
+
+export default useMountStatus;
