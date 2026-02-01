57b482e5a84b03ca5264e22f6f07df679521a25a
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-02 01:01:21 +0530

    Added update video meta data

---
+++ src/scripts/updateVideoMetaData.ts
@@ -0,0 +1,30 @@
+import db from '../db/index';
+import { durationMetaData, DurationMetaData } from './data';
+
+
+async function updateVideoMetaData(data: DurationMetaData[]){
+    try{
+        await db.$transaction(async (tx) => {
+            for(const { duration, id } of data){
+                if(typeof duration === 'number'){
+                    await tx.videoMetadata.update({
+                        where: {
+                            contentId: id
+                        },
+                        data: {
+                            duration: duration as number
+                        }
+                    });
+                }
+            }
+        });
+        await db.$disconnect();
+        console.log('All updates completed successfully.');
+    } catch(e){
+        console.error('Error occurred during updates:', e);
+        await db.$disconnect();
+        process.exit(1);
+    }
+}
+
+updateVideoMetaData(durationMetaData);
