05b9927996f71a88ba340a718fd3a58c2be53397
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-02 22:15:23 +0530

    Added setup

--- src/tests/helpers/setup.ts
+++ src/tests/helpers/setup.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/extension";
 import '@testing-library/jest-dom';
 import { beforeAll, beforeEach, afterAll, afterEach } from 'vitest';
 
